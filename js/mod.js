35fec0523d119316dd6050d4d6b799f9985e1dac
unlimit1408 <157990990+unlimit1408@users.noreply.github.com>
2024-06-23 00:01:50 -0400

    Update mod.js
    Added mod name

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Big Tree",
+	id: "mymod1408",
+	author: "unlimit1408",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
